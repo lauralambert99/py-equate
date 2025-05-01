71fb9b42be99397c6229d6fcfda0c4b3c0f35246
lauralambert99 <laycocla@jmu.edu>
2025-05-01 07:49:57 -0400

    Added methods folder, created tucker.py file and added __init__.py for eventual packaging.

---
+++ Methods/Tucker.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 30 11:30:41 2025
+
+@author: laycocla
+"""
+import pandas as pd
+import numpy as np
+
+def Tucker(x, y, common_x, common_y, scores, w1):
+    """
+  Perform Tucker equating.
+
+  Parameters:
+  x, y: Array of raw scores for Form X and Form Y
+  common_x, common_y: Arrays of anchor scores for each form
+  scores: Array of score range to equate
+  w1: Weight for group 1
+
+  Returns:
+  DataFrame of equated scores
+  """
+  
+    #Define weights
+    w2 = (1 - w1)
+    
+    #Calculate gamma
+    gamma_1 = common_x.cov(x/common_x.var())
+    gamma_2 = common_y.cov(y/common_y.var())
+
+    #Synthetic population stuff
+    mu_sx = np.mean(x) - w2*gamma_1*(np.mean(common_x) - np.mean(common_y))
+    mu_sy = np.mean(y) + w1*gamma_2*(np.mean(common_x) - np.mean(common_y))
+
+    var_sx = (x.var() - w2*(gamma_1**2)*(common_x.var() - common_y.var()) + w1*w2*(gamma_1**2)*(np.mean(common_x) - np.mean(common_y))**2)
+    var_sy = (y.var() + w1*(gamma_2**2)*(common_x.var() - common_y.var()) + w1*w2*(gamma_2**2)*(np.mean(common_x) - np.mean(common_y))**2)
+
+    #Get standard deviations
+    sd_sx = np.sqrt(var_sx)
+    sd_sy = np.sqrt(var_sy)
+    
+    ly_x = (sd_sx/sd_sy)*(scores - mu_sx) + mu_sy
+
+    eyx = pd.DataFrame({'Scores': scores,
+                       'ey': ly_x})
+    return eyx
---
+++ Methods/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  1 07:45:30 2025
+
+@author: laycocla
+"""
+
