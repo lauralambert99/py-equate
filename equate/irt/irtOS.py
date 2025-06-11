a8dcce131469047ef80595e228cc4cfda2ed40ab
lauralambert99 <laycocla@jmu.edu>
2025-06-11 09:48:40 -0400

    edits to IRT functions; adding in place-holder for IRT-OS

---
+++ equate/irt/irtOS.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 11 09:44:06 2025
+
+@author: laycocla
+"""
+
+#Use BILOG-MG
+#Get OS distributions for Form X and Y
+#Get synthetic population
+#Do equipercentile equating
--- equate/irt/transf.py
+++ equate/irt/transf.py
@@ -8,19 +8,22 @@ Created on Wed Jun 11 08:50:28 2025
 import numpy as np
 import pandas as pd
 
-def transf(aJ, bJ, aI, bI, method = "mean_mean"):
+#TODO: I bet we can require a dataframe with specified columns to make this neater.
+
+def transf(aJ, bJ, cJ, aI, bI, items, common, method = "mean_mean"):
     """
   Perform mean/mean scale transformation.
 
   Parameters:
-  x, y: Array of raw scores for Form X and Form Y
-  common_x, common_y: Arrays of anchor scores for each form
-  scores: Array of score range to equate
+  aJ, bJ, cJ: Array of item parameters for the old form
+  aI, bI: Array of item parameters for the new form
+  common: Array of common items (e.g., 1:5)
+  items: Array of item numbers (e.g., 1, 2, 3, etc.)
   
   method: what scale transformation to use; select from "mean_mean", "mean_sigma", "S_L" (Stocking & Lord), or "Haebara"
 
   Returns:
-  ????
+  A dataframe with item parameters on the old and new forms, and the transformed parameters.
   """
 #First, need a and b common item parameters to do the transformation
 
@@ -28,26 +31,7 @@ def transf(aJ, bJ, aI, bI, method = "mean_mean"):
     valid_methods = ["mean_mean", "mean_sigma", "S_L", "Haebara"]
     if method not in valid_methods:
         raise ValueError(f"Method '{method}' not supported. Choose from {valid_methods}")
-        
-    if method == "mean_mean":
-        A = np.std(bJ)/np.std(bI)
-        B = np.mean(bJ) - A*np.mean(bI)
     
-    elif method == "mean_sigma":
-        A = np.mean(aI)/np.mean(aJ)
-        B = np.mean(bJ) - A*np.mean(bI)
-    
-    elif method == "S_L":
-        do stuff
-    
-    elif method = "Haebara":
-        do other stuff
-    
-    else:
-        raise ValueError(f"Unsupported method: {method}")
-  
-    #To do rescaling:
-    #Brain is not braining: put form I onto form J to get form J equivalents.....WHICH ONE ARE WE CHANGING???
     data = pd.DataFrame({
         'Item': items, 
         'aJ': aJ, 
@@ -58,8 +42,31 @@ def transf(aJ, bJ, aI, bI, method = "mean_mean"):
         'cI': cJ,
     })
     
+    #TODO: specify only do these things on common items!
+    
+    if method == "mean_mean":
+        A = np.std(bJ)/np.std(bI)
+        B = np.mean(bJ) - A*np.mean(bI)
+    
+    elif method == "mean_sigma":
+        A = np.mean(aI)/np.mean(aJ)
+        B = np.mean(bJ) - A*np.mean(bI)
+    
+    #elif method == "S_L":
+        #do stuff iteratively using Gauss-Hermite quadrature to approximate the integral in the minimized function
+    
+    #elif method = "Haebara":
+       #do other stuff iteratively
+    
+    else:
+        raise ValueError(f"Unsupported method: {method}")
+  
+    #To do rescaling:
+    #Brain is not braining: put form I onto form J to get form J equivalents.....WHICH ONE ARE WE CHANGING???
+
+    
     data['aJ_transf'] = aI/A
-    data['bJ_transf'] = A*bIj + B
+    data['bJ_transf'] = A*bI + B
     
     thetaJ = A*thetaI + B
     
