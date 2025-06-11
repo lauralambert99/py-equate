c1bce26d0746828dced42ca84b25dbeedd9228b4
lauralambert99 <laycocla@jmu.edu>
2025-06-11 09:50:10 -0400

    Changed BILOG-MG to pyirt

--- equate/irt/irtOS.py
+++ equate/irt/irtOS.py
@@ -5,7 +5,7 @@ Created on Wed Jun 11 09:44:06 2025
 @author: laycocla
 """
 
-#Use BILOG-MG
+#Use pyirt (BILOG-MG not free)
 #Get OS distributions for Form X and Y
 #Get synthetic population
 #Do equipercentile equating
