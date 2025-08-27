8c2aed5b8c166afe6c2adc97f09b509fff3a05da
lauralambert99 <laycocla@jmu.edu>
2025-08-27 05:20:56 -0400

    Updating setup file

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='equate',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=['numpy', 'pandas']
+    version='0.1.0',
+    packages = find_packages(),
+    description="Score equating methods for testing",
+    authors=['Laura Lambert', 'Yu Bao'],
+    install_requires=['numpy', 'pandas', 'scipy', 'statsmodels']
 )
