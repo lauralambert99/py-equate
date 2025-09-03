179721574d109074e9cacffb377a547daed62561
lauralambert99 <laycocla@jmu.edu>
2025-09-03 11:09:29 -0400

    edit to see init file

--- equate/se_equating/__init__.py
+++ equate/se_equating/__init__.py
@@ -6,5 +6,6 @@ Created on Mon Jun 23 22:15:26 2025
 """
 from .linear_se import linear_se
 from .mean_se import mean_se
+from .equi_se import eq_see_asy
 
-__all__ = ['linear_se', 'mean_se']
+__all__ = ['linear_se', 'mean_se', 'eq_see_asy']
